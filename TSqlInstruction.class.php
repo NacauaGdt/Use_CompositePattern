1176388fada48e42b938da4e9eb8e0bd57d0dffe
diaspedro7 <gerhardtgaroto@gmail.com>
2024-09-30 14:49:28 -0300

    Create TSqlInstruction.class.php

---
+++ TSqlInstruction.class.php
@@ -0,0 +1,42 @@
+<?php
+
+//classe TSqlInstruction
+//Classe que prove os metodos em comum entre todas as instrucoes
+//SQL(SELECT, INSERT, DELETE e UPDATE)
+
+abstract class TSqlInstruction {
+
+    protected $sql; //armazena a instrucao sql
+    protected $criteria; // armazena o objeto criterio
+    protected $entity; //armazena a tabela do banco de dados
+
+    //metodo setEntity()
+    //define o nome da entidade (tabela) a ser manipulada pela instrucao SQL
+    //parametro $entity = tabela
+
+
+  final public function setEntity($entity)
+    {
+        $this->entity = $entity;
+    }
+
+    final   public function getEntity()
+    {
+        return $this->entity;
+    }
+    
+    public function setCriteria($criteria)
+    {
+        $this->criteria = $criteria;
+    }
+
+    //metodo getInstruction()
+    //declarando o como abstract obrigamos a declaracao nas classes filhas
+    //uma vez que seu comportamento sera distinto em cada uma delas, configurando polimorfismo
+
+
+    abstract public function getInstruction();
+
+}
+
+?>
